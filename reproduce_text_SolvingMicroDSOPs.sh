64df8aa8a12d3caf54061ddaa1fd413d6ad8b62f
llorracc <git@llorracc.org>
2024-06-10 07:03:15 -0400

    worked

--- reproduce_text_SolvingMicroDSOPs.sh
+++ reproduce_text_SolvingMicroDSOPs.sh
@@ -1,6 +1,2 @@
 #/bin/bash
-pdflatex -interaction=nonstopmode                           -halt-on-error -file-line-error -shell-escape \\newcommand\\UseOption{FromShell}\\input{SolvingMicroDSOPs}
-bibtex -terse    SolvingMicroDSOPs
-pdflatex -interaction=nonstopmode                           -halt-on-error -file-line-error -shell-escape \\newcommand\\UseOption{FromShell}\\input{SolvingMicroDSOPs}
-pdflatex -interaction=nonstopmode                           -halt-on-error -file-line-error -shell-escape \\newcommand\\UseOption{FromShell}\\input{SolvingMicroDSOPs}
-latexmk -c
+scriptDir="$(realpath $(dirname $0))" ; cd $scriptDir
